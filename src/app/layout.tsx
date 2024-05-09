23c50ffca6022624fb91d685bb65bf7fc69dade6
ChristianPickettCode <pickett.christian@outlook.com>
2024-05-09 17:19:18 +0200

    add pageview analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Open_Sans } from "next/font/google";
 import "./globals.css";
 import { CSPostHogProvider } from './providers'
+import PostHogPageView from "./PostHogPageView";
 
 // "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif
 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <CSPostHogProvider>
         <body className={openSans.className}>
+          <PostHogPageView />
           {children}
         </body>
       </CSPostHogProvider>
